13486fdd2f38eb84bd99fa54b9a2c4287f0bf901
XiaoMing18 <2460903407@qq.com>
2019-09-03 10:13:10 +0800

    顺序结构的概述及实际应用

---
+++ src/Chapter3_ProgramFlowControlStatement/Section01_SequentialStructureStatement/SequentialStructure_01.java
@@ -0,0 +1,19 @@
+package Chapter3_ProgramFlowControlStatement.Section01_SequentialStructureStatement;
+//Sequential structure:顺序结构
+/**
+ * 流程控制语句:
+ * 概述:控制程序的执行流程
+ * 分类:
+ *     顺序结构
+ *     选择结构
+ *     循环结构
+ *
+ * 顺序结构:从上往下依次执行
+ */
+public class SequentialStructure_01 {
+    public static void main(String[] args) {
+        System.out.println("开始");
+        System.out.println("进行");
+        System.out.println("结束");
+    }
+}
