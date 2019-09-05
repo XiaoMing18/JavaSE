150d7675b38def1851aa4d20cfff2dc316b8d1fb
XiaoMing18 <2460903407@qq.com>
2019-09-05 17:47:14 +0800

    返回值为void类型的方法练习   需求   2,键盘录入行数和列数输出对应的星型

---
+++ src/Chapter05/Function/Section08_VoidPratice2.java
@@ -0,0 +1,32 @@
+package Chapter05.Function;
+//返回值为void类型的方法练习
+
+/**
+ * 需求
+ * 1,键盘录入行数和列数输出对应的星型
+ */
+
+
+
+import java.util.Scanner;
+
+public class Section08_VoidPratice2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入行数");
+        int m = sc.nextInt();
+        System.out.println("请输入列数");
+        int n = sc.nextInt();
+//单独调用
+star(m,n);
+    }
+public static void star(int m,int n){
+        for (int x = 0;x < m;x++){
+            for (int y = 0;y < n;y++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+}
+    }
+
