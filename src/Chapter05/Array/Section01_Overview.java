19be6978e6c25f9a97622f4dddeab83cb45a3434
XiaoMing18 <2460903407@qq.com>
2019-09-05 17:44:27 +0800

    数组概述和定义格式说明

---
+++ src/Chapter05/Array/Section01_Overview.java
@@ -0,0 +1,33 @@
+package Chapter05.Array;
+//Array overview:数组概述
+//数组概述和定义格式说明
+
+/**
+ * 数组:存储同一种数据类型的多个元素的容器
+ * 定义格式:
+ *    A:数据类型 [] 数组名;
+ *    B:数据类型 数组名 [];
+ *
+ *
+ * 举例:
+ *     A:int [] a:定义一个int类型的数组a变量
+ *     B:int a []:定义一个int类型的a数组变量
+ *
+ * 注意:
+ *     效果可以认为是一样的,
+ *     都是定义一个int数组,
+ *     但是念法上有些小区别,推荐使用第一种
+ */
+
+
+
+
+public class Section01_Overview {
+    public static void main(String[] args) {
+
+
+    }
+
+
+    }
+
