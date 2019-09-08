c339fe43178a106298ddbcfb57de3e632595cf7c
XiaoMing18 <2460903407@qq.com>
2019-09-08 17:31:53 +0800

    for循环练习:   1请在控制台输出1到10

---
+++ src/Chapter3_ProgramFlowControlStatement/Section04_For/Node02_ForPracticeGetData.java
@@ -0,0 +1,13 @@
+package Chapter3_ProgramFlowControlStatement.Section04_For;
+//获取数据
+/**
+ * for循环练习:
+ * 1请在控制台输出1到10
+ */
+public class Node02_ForPracticeGetData {
+    public static void main(String[] args) {
+        for (int x = 1;x <= 10;x++ ){
+            System.out.println("循环打印第"+ x + "次");
+        }
+    }
+}
