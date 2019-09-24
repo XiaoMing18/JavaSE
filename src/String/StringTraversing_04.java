90e3f8c2349fedd42e7889855396f950562fc243
XiaoMing18 <2460903407@qq.com>
2019-09-24 17:40:54 +0800

    遍历字符串

--- src/String/StringTraversing_04.java
+++ src/String/StringTraversing_04.java
@@ -2,6 +2,27 @@ package String;
 import java.util.Scanner;
 /**
  * 遍历字符串
+ * 需求:键盘录入一个字符串,使用程序实现在控制台遍历输入的字符串
+ *
+ * 思路:
+ *
+ *        1,键盘录入一个字符串,使用Scanner实现
+ *
+ *        2,遍历键盘输入的字符串，需要获得字符串中每一个字符
+ *
+ * - public char charAt(int index):返回指定索引处的char值,字符串的索引从0开始
+ *
+ *
+ *
+ *       3,遍历字符串,获取到字符串的长度
+ *
+ * - public int length():返回此字符串的长度
+ *
+ * - 数组的长度:数组名.length
+ * - 字符串长度:字符串对象.length()
+ *
+ *       4,遍历字符串的通用格式
+ *
  *
  */
 public class StringTraversing_04 {
