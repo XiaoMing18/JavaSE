297945f8afb91c2976fb1c0f071fdc0a02a59279
XiaoMing18 <2460903407@qq.com>
2020-06-10 09:58:13 +0800

    一维数组的初始化

---
+++ src/Chapter06_Array/day_01/LinearArrayInitialize_01.java
@@ -0,0 +1,35 @@
+package Chapter06_Array.day_01;
+
+/**
+ * LinearArrayInitialize：一维数组初始化
+ *
+ * 一维数组的使用
+ * 1,一维数组的声明和初始化
+ * 2,如何调用数组指定位置的元素
+ * 3,如何获取数组长度
+ * 4,如何遍历数组
+ * 5,数组的默认初始化值
+ * 6,数组的内存解析
+ *
+ */
+public class LinearArrayInitialize_01 {
+    public static void main(String[] args) {
+        //1一维数组的声明和初始化
+        int num;//声明
+         num = 100;//初始化
+        int id = 1000;//声明+初始化
+
+
+        int [] ids;//声明
+        //1.1静态初始化:数组的初始化和数组元素的赋值同时进行
+        ids =new int[] {1001,1002,1003,1004,1005};
+        //1.2动态初始化:数组的初始化和数组元素的赋值分开进行
+        String[] names = new String[4];
+
+
+        //总结:数组一旦初始化完成,长度就确定了
+
+
+
+    }
+}
