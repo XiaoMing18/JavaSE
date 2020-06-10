c97b9d51fb8284e9d9c71c9ffe97599deaabb23a
XiaoMing18 <2460903407@qq.com>
2020-06-10 15:04:10 +0800

    数组内存解析

---
+++ src/Chapter06_Array/day_01/ArrayMemoryAnalysis_03.java
@@ -0,0 +1,17 @@
+package Chapter06_Array.day_01;
+
+/**
+ * ArrayMemoryAnalysis:数组内存解析
+ *
+ * 栈(stack):局部变量
+ *
+ * 堆(heap):new出来的结构:对象,数组
+ *
+ * 方法区:常量池，静态域
+ *
+ */
+public class ArrayMemoryAnalysis_03 {
+    public static void main(String[] args) {
+
+    }
+}
