4ad68acf69ed4a5d917a3d391034890e3aa26507
XiaoMing18 <2460903407@qq.com>
2020-06-22 08:44:17 +0800

    内存解析的说明

---
+++ src/Chapter08_ObjectOrientedTop/AnonymousObject_02.java
@@ -0,0 +1,15 @@
+package Chapter08_ObjectOrientedTop;
+
+/**
+ * 内存解析的说明
+ * 1,引用类型的变量,只可能存储两类值:null或地址值(含变量类型)
+ */
+public class AnonymousObject_02 {
+    public static void main(String[] args) {
+
+    }
+}
+class Student{
+    String name;
+    int age;
+}
